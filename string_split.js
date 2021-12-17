9c2d20ed666ab7919da3e7e10a10742f21355f8e
Petar <baki_pfc@yahoo.com>
2021-12-17 23:37:07 +0100

    propose pseudocode solution

--- string_split.js
+++ string_split.js
@@ -1,3 +1,12 @@
 // String Split challenge
 // Problem: You have a long string over some alphabet and a large dictionary of words over the same alphabet.
-// Solution: Suggets an algorith to split the String into words from the dictionary; if possible print the String with spaces accordingly. If not possible, return FALSE.
+// Solution: Suggets an algorithm to split the String into words from the dictionary; if possible print the String with spaces accordingly. If not possible, return FALSE.
+
+
+// SOLUTION IN PSEUDOCODE
+
+// - Define a long String.
+// - Define an array of words.
+// - Identify helpful String and Array methods/regular expressions for this particular case.
+// - Bake a function that'll take a String as an input & return a String of matched words on success or return false if no words matched.
+
