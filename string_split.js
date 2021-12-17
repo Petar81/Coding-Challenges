e5573c86fb0a12954d2408881cda81a934f01317
Petar <baki_pfc@yahoo.com>
2021-12-17 22:49:23 +0100

    String Split challenge

---
+++ string_split.js
@@ -0,0 +1,3 @@
+// String Split challenge
+// Problem: You have a long string over some alphabet and a large dictionary of words over the same alphabet.
+// Solution: Suggets an algorith to split the String into words from the dictionary; if possible print the String with spaces accordingly. If not possible, return FALSE.
