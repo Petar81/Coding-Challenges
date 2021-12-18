eeaf1ab9f96dc9317cde34f255ba7de102c92e22
Petar <baki_pfc@yahoo.com>
2021-12-18 19:15:48 +0100

    propose solution in pseudocode

--- circular_identity.js
+++ circular_identity.js
@@ -1,3 +1,12 @@
 // CIRCULAR IDENTITY CHALLENGE
 // Problem: Find out if two different Strings are circularly identical.
 // Solution: Implement a function that checks if two Strings (eg. "aabbb" & "bbaab") are circularly identical.
+
+
+
+// SOLUTION IN PSEUDOCODE
+
+// - Define two different Strings.
+// - Identify helpful String and/or Chracter looping/iteration mechanism(s)
+// - Bake a function that'll take two different Strings as an arguments, compare one to each other & return "IDENTICAL" if the Strings consist of the same characters (but not necessarily in the same order) otherwise return "NON IDENTICAL".
+
