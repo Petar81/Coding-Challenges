8dc7f80563be27c6715b43f964640c2f0eb381ee
Petar <baki_pfc@yahoo.com>
2021-12-18 18:30:30 +0100

    Circular Identity Challenge

---
+++ circular_identity.js
@@ -0,0 +1,3 @@
+// CIRCULAR IDENTITY CHALLENGE
+// Problem: Find out if two different Strings are circularly identical.
+// Solution: Implement a function that checks if two Strings (eg. "aabbb" & "bbaab") are circularly identical.
