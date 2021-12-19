58ab17d7174b070e84ae430a3a804337c652dfdb
Petar <baki_pfc@yahoo.com>
2021-12-19 20:47:44 +0100

    propose solution in pseudocode

--- circular_identity.js
+++ circular_identity.js
@@ -7,7 +7,7 @@
 // SOLUTION IN PSEUDOCODE
 
 // - Define two different Strings.
-// - Identify helpful String and/or Chracter looping/iteration mechanism(s)
+// - Identify helpful String and/or Character looping/iteration mechanism(s)
 // - Bake a function that'll take two different Strings as an arguments, compare one to each other & return "IDENTICAL" if the Strings consist of the same characters (but not necessarily in the same order) otherwise return "NON IDENTICAL".
 
 
--- reverse_string.js
+++ reverse_string.js
@@ -1,3 +1,13 @@
 // REVERSE STRING challenge
-// Problem: Take a string and reverse it
+// Problem: Take a string and reverse it.
 // Solution: Implement an algorithm that reverses a string (e.g. "YourName > "emaNrouy") - sort of a mirror effect.
+
+
+
+// SOLUTION IN PSEUDOCODE
+
+// - Define a string to be reversed.
+// - Identify helpful String and/or Character manipulation methods.
+// - Bake a function that'll take a String as an input and reverse it.
+
+
