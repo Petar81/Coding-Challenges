388627b056209b0c4d6a8860e7c04fba842fc50c
Petar <baki_pfc@yahoo.com>
2021-12-20 03:01:37 +0100

    propose solution in pseudocode

--- fizzbuzz_modulo.js
+++ fizzbuzz_modulo.js
@@ -4,3 +4,12 @@
 
 
 
+// SOLUTION IN PSEUDOCODE
+
+// - Define a const that holds a number (integer).
+// - Use modulo operator to support the case.
+// - Bake a function that'll take an integer as an input and Print Fizz if number is divisible by 3, print Buzz if it's divisible by 5 and otherwise print "i".
+
+
+
+
