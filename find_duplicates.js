093217481ed9849cc47107300052672d61d7b179
Petar <baki_pfc@yahoo.com>
2021-12-21 00:45:35 +0100

    Coding Solution using filter() method

--- find_duplicates.js
+++ find_duplicates.js
@@ -9,3 +9,14 @@
 // - Define an array to search through.
 // - Identify helpful array methods.
 // - Bake a function that'll take an Array as an input and return duplicate number(s).
+
+
+
+
+// CODING SOLUTION - super simple solution with the help of a filter() method
+// Returns an array with all the numbers that pass the duplicacy test
+// If no elements pass the test it returns an emprty array
+
+const numbers = [3, 1, 1, 3];
+let hasDuplicate = numbers.filter((number, index) => numbers.indexOf(number) !== index);
+console.log(hasDuplicate);
