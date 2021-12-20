dc786c80a9b379b83753e8a6fce4fbc74b9bdc37
Petar <baki_pfc@yahoo.com>
2021-12-20 20:09:48 +0100

    propose solution in pseudocode

--- string_to_integer.js
+++ string_to_integer.js
@@ -1,3 +1,11 @@
 // Challenge: convert a String to an Integer.
 // Problem: non numeric chracters should throw an error.
 // Solution: Implement a function that takes a "numerical String" as an input and converts it to an Integer;  otherwise throw an error.
+
+
+
+// SOLUTION IN PSEUDOCODE
+
+// - Define a String to be converted to Integer value.
+// - Identify helpful String checking/matching methods. 
+// - Bake a function that'll convert a String to an Integer or throw an error if non numerical character found.
