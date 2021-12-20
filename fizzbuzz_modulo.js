6c8e6c72c7b04ad761e3b524dd0e9b9b26a6a052
Petar <baki_pfc@yahoo.com>
2021-12-20 02:56:47 +0100

    FizzBuzz Modulo challenge

---
+++ fizzbuzz_modulo.js
@@ -0,0 +1,6 @@
+// Challenge: Print Fizz if number is divisible by 3, print Buzz if it's divisible by 5 and otherwise print "i".
+// Problem: Can an arbitrary number (integer) be divide by 3 and/or 5?
+// Solution: Implement a function that uses modulo operator to find out if an integer we pass as an argument is divisible by 3 and/or 5.
+
+
+
