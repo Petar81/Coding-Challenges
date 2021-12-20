d8d6f2e4ca587c042eb1a78def838019d3c1cfae
Petar <baki_pfc@yahoo.com>
2021-12-20 19:35:10 +0100

    Convert a Strinig to an Integer challenge

---
+++ string_to_integer.js
@@ -0,0 +1,3 @@
+// Challenge: convert a String to an Integer.
+// Problem: non numeric chracters should throw an error.
+// Solution: Implement a function that takes a "numerical String" as an input and converts it to an Integer;  otherwise throw an error.
