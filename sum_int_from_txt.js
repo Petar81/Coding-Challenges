2fa25693cb12772db835cc86e7d0c33058b2cba0
Petar <baki_pfc@yahoo.com>
2021-12-20 15:51:43 +0100

    Coding solution with for loop

--- sum_int_from_txt.js
+++ sum_int_from_txt.js
@@ -9,3 +9,33 @@
 // - Define an input field (input file).
 // - Identify file reader function/class, identify string split method, identify parse number/integer method. 
 // - Bake a function that'll take a file as an input, parse it line by line and sum all the numbers.
+
+
+
+
+// CODING SOLUTION WITH FOR LOOP
+
+<input type="file" name="file" id="file"></input> // HTML part
+
+document.getElementById('file').onchange = function(){
+    var file = this.files[0];
+    var sum = 0;
+    var fileReader = new FileReader();
+    fileReader.onload = function(progress){
+      // get the lines
+      var lines = this.result.split('\n');
+      // loop through the lines
+      for(var line = 0; line < lines.length; line++){
+        // console.log(lines[line]);
+        sum += Number(lines[line]);
+      }
+    }
+    fileReader.readAsText(file);
+    console.log(sum);
+  };
+
+
+
+
+
+
