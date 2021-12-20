19901be8bfe2bafd19a585cb86c11eec0269ec8c
Petar <baki_pfc@yahoo.com>
2021-12-21 00:40:07 +0100

    propose solution in pseudocode

--- find_duplicates.js
+++ find_duplicates.js
@@ -1,3 +1,11 @@
 // FIND DUPLICATE(S) challenge
 // Problem: spot duplicate(s) value in an array.
 // Solution: Implement an algorithm that finds duplicate value(s) in an array.
+
+
+
+// SOLUTION IN PSEUDOCODE
+
+// - Define an array to search through.
+// - Identify helpful array methods.
+// - Bake a function that'll take an Array as an input and return duplicate number(s).
