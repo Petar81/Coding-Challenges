af2811b753c5832db9aa7b25943d495ff39dd246
Petar <baki_pfc@yahoo.com>
2021-12-20 15:27:10 +0100

    Sum Up Integers from a text file challenge

---
+++ sum_int_from_txt.js
@@ -0,0 +1,6 @@
+// Challenge: Sum up integers from a text file, line by line.
+// Problem: Numbers are in a different (input) file scattered across multiple lines.
+// Solution: Implement a function that takes file as an input, reads it line by line and sums all the numbers.
+
+
+
