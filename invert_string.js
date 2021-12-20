6df231234d1250e3b61c8792b76650dd400d2cfa
Petar <baki_pfc@yahoo.com>
2021-12-20 01:57:13 +0100

    propose solution in pseudocode

--- invert_string.js
+++ invert_string.js
@@ -1,3 +1,11 @@
 // INVERT STRING challenge
 // Problem: Take a string and invert it.
 // Solution: Implement an algorithm that inverts a string (e.g. "YoUr Name" > "yOuR nAME").
+
+
+
+// SOLUTION IN PSEUDOCODE
+
+// - Define a string to be inverted.
+// - Identify helpful String and/or Character manipulation methods.
+// - Bake a function that'll take a String as an input and invert it.
