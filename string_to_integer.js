a11fb20a061020b4b4f00402c4c050bee1c0c160
Petar <baki_pfc@yahoo.com>
2021-12-20 20:17:59 +0100

    Coding Solution String to Integer using match(regex)

--- string_to_integer.js
+++ string_to_integer.js
@@ -6,6 +6,25 @@
 
 // SOLUTION IN PSEUDOCODE
 
-// - Define a String to be converted to Integer value.
+// - Define a String to be converted to an Integer value.
 // - Identify helpful String checking/matching methods. 
 // - Bake a function that'll convert a String to an Integer or throw an error if non numerical character found.
+
+
+
+// CODING SOLUTION USING match(regex) METHOD (any non numerical character at any position will make code throw an error)
+
+function stringToInteger(str) {
+
+    if (str.match("^[0-9]+$")) {
+        console.log(parseInt(str));
+    } else {
+        throw 'Parameter is not a number!';
+    }
+}
+
+try {
+    stringToInteger('88 # !17');
+} catch (e) {
+    console.log(e); // Parameter is not a number!
+}
