e333701dcb1bb6479b01728d6851d41471a0e4a5
Petar <baki_pfc@yahoo.com>
2021-12-20 01:55:26 +0100

    Invert String challenge

---
+++ invert_string.js
@@ -0,0 +1,3 @@
+// INVERT STRING challenge
+// Problem: Take a string and invert it.
+// Solution: Implement an algorithm that inverts a string (e.g. "YoUr Name" > "yOuR nAME").
--- reverse_string.js
+++ reverse_string.js
@@ -1,6 +1,6 @@
 // REVERSE STRING challenge
 // Problem: Take a string and reverse it.
-// Solution: Implement an algorithm that reverses a string (e.g. "yourName > "emaNruoy") - sort of a mirror effect.
+// Solution: Implement an algorithm that reverses a string (e.g. "yourName" > "emaNruoy") - sort of a mirror effect.
 
 
 
