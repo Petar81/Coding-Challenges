8b292f9e322a2eb2c3732dd94a60e772017922e1
Petar <baki_pfc@yahoo.com>
2021-12-20 02:28:33 +0100

    Invert String Coding Solution

--- invert_string.js
+++ invert_string.js
@@ -9,3 +9,39 @@
 // - Define a string to be inverted.
 // - Identify helpful String and/or Character manipulation methods.
 // - Bake a function that'll take a String as an input and invert it.
+
+
+
+
+// CODING SOLUTION
+
+
+function invertString(str) {
+    let strLength = str.length;
+    let invertedString = [];
+
+    for (let i = 0; i < strLength; i++) {
+
+        if (str[i] >= "A" && str[i] <= "Z")
+            // console.log(str[i] + " is an UpperCase character");
+            invertedString[i] = str[i].toLowerCase();
+        else if (str[i] >= "a" && str[i] <= "z")
+            // console.log(str[i] + " is a LowerCase character");
+            invertedString[i] = str[i].toUpperCase();
+        else if (str[i] === " ") {
+           // console.log(str[i] + " is a space character");
+           invertedString[i] = " ";
+        } else {
+            // console.log(str[i] + " is not an aplhabetic character");
+            continue;
+        }
+    }
+
+    console.log(invertedString.join("")); // yOuR nAME
+
+}
+
+
+var str = "YoUr Name";
+
+invertString(str);
