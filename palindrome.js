a51e86710c56982bf2636f58e19cacc77f4e8a62
Petar <baki_pfc@yahoo.com>
2021-12-21 15:31:53 +0100

    Palindrome Coding challenge

---
+++ palindrome.js
@@ -0,0 +1,3 @@
+// Challenge: check if a String is a Palindrome - reads the same backwards & forwards.
+// Problem: find the same forwards/backwards reading pattern in one single String. 
+// Solution: Implement a function that takes a String & logs Palindrome if a word reads the same backwards & forwards.
