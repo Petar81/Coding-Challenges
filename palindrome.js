819ff9379e56289eddb2488212fa29b6464ee005
Petar <baki_pfc@yahoo.com>
2021-12-21 15:48:38 +0100

    Palindrome Coding Solution

--- palindrome.js
+++ palindrome.js
@@ -9,3 +9,23 @@
 // - Define a String to be checked against the algorithm.
 // - Reverse an input String and save it as a local variable & then compare it with the original input String.
 // - Bake a function that will take a String as an input, apply the afore mentioned algorithm & print out a reulst.
+
+
+
+
+// CODING SOLUTION
+
+function isPalindrome(str){
+    let palindrome = str.split("").reverse().join("");
+    
+    if(str.toLowerCase() === palindrome.toLowerCase()){
+     console.log(str + " is palindrome.");
+    } else console.log(str + " is NOT palindrome.");
+    
+    }
+    
+    isPalindrome('Racecar'); // Racecar is palindrome (note: 1st letter is Capital but the algorithm still recognizes a palindrome).
+    isPalindrome('madam'); // madam is palindrome.
+    isPalindrome('11/11/11'); // 11/11/11 is palindrome (note: Dates and Numbers as Strings are recognized as palindromes as well).
+    isPalindrome('jojo'); // jojo is NOT palindrome.
+    isPalindrome('David'); // David is NOT palindrome.
