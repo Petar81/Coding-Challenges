6be670edd18a9fb57cf226867517a38cb1c32f29
Petar <baki_pfc@yahoo.com>
2021-12-21 01:15:22 +0100

    Coding Solution using Math.abs() method

--- find_duplicates.js
+++ find_duplicates.js
@@ -13,10 +13,42 @@
 
 
 
-// CODING SOLUTION - super simple solution with the help of a filter() method
+// CODING SOLUTION - super simple solution using the help of a filter() method
 // Returns an array with all the numbers that pass the duplicacy test
 // If no elements pass the test it returns an emprty array
 
 const numbers = [3, 1, 1, 3];
 let hasDuplicate = numbers.filter((number, index) => numbers.indexOf(number) !== index);
-console.log(hasDuplicate);
+console.log(hasDuplicate); // [ 1, 3 ]
+
+
+
+
+// CODING SOLUTION - using the help of Math.abs() method
+// Returns ONLY the first duplicate it spots
+
+const elements = [3, 1, 1, 3];
+const elementsLength = elements.length;
+
+function findDuplicate(elements, elementsLength) {
+    let duplicateElement = 0;
+
+    for (let i = 0; i < elementsLength; i++) {
+
+        let element = elements[Math.abs(elements[i])];
+        // console.log('element ' + element);
+
+        if (element < 0) {
+            duplicateElement = elements[i];
+            break;
+        }
+
+        elements[Math.abs(elements[i])] = -elements[Math.abs(elements[i])];
+        // console.log('element value ' + elements[Math.abs(elements[i])]);
+    }
+
+    return Math.abs(duplicateElement);
+
+}
+
+console.log('Duplicate has been found and it has a value of ' + findDuplicate(elements, elementsLength));
