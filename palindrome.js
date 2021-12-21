4ffc3169814e4ec18da20abca35065cb6a5fa180
Petar <baki_pfc@yahoo.com>
2021-12-21 15:37:38 +0100

    propose solution in pseudocode

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,11 @@
 // Challenge: check if a String is a Palindrome - reads the same backwards & forwards.
 // Problem: find the same forwards/backwards reading pattern in one single String. 
 // Solution: Implement a function that takes a String & logs Palindrome if a word reads the same backwards & forwards.
+
+
+
+// SOLUTION IN PSEUDOCODE
+
+// - Define a String to be checked against the algorithm.
+// - Reverse an input String and save it as a local variable & then compare it with the original input String.
+// - Bake a function that will take a String as an input, apply the afore mentioned algorithm & print out a reulst.
